c596e51b08b8e571f136d8925f8815d48de2adb3
= <rakib.ivan100@gmail.com>
2022-04-08 23:42:17 +0600

    commit done

--- Model/registration.php
+++ Model/registration.php
@@ -22,45 +22,45 @@
             $confirmpassword = $_POST["confirmpassword"];
         }
         // if there are erros
-        if(empty($_POST["firstname"]))  
-        {   
-           $firstNameError ="<label>Firsr name Required</label>";
-        }
+        // if(empty($_POST["firstname"]))  
+        // {   
+        //    $firstNameError ="<label>Firsr name Required</label>";
+        // }
 
-        if(empty($_POST["lastname"]))  
-        {  
-            $lastNameError ="<label>last name required</label>";
-        }
+        // if(empty($_POST["lastname"]))  
+        // {  
+        //     $lastNameError ="<label>last name required</label>";
+        // }
 
-        if (empty($_POST["email"])) 
-        {
-            $emailError ="<label>Email Required</label>";
-        } 
-        else 
-        {   
-            $email= $_POST['email'];
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
-            {
-                $emailFormatError ="<label>Invalid email format</label>";
-            }
-        }
-        if(empty($_POST["username"]))  
-        {  
-            $usernameError ="<label>Username Required</label>"; 
-        }
-        if(empty($_POST["password"]))  
-        {  
-            $passwordError ="<label>Password Required</label>"; 
-        }
+        // if (empty($_POST["email"])) 
+        // {
+        //     $emailError ="<label>Email Required</label>";
+        // } 
+        // else 
+        // {   
+        //     $email= $_POST['email'];
+        //     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        //     {
+        //         $emailFormatError ="<label>Invalid email format</label>";
+        //     }
+        // }
+        // if(empty($_POST["username"]))  
+        // {  
+        //     $usernameError ="<label>Username Required</label>"; 
+        // }
+        // if(empty($_POST["password"]))  
+        // {  
+        //     $passwordError ="<label>Password Required</label>"; 
+        // }
         
-        if(empty($_POST["confirmpassword"]))  
-        {  
-            $confirmpasswordError ="<label>Confirm Password Required</label>";  
-        } 
-        else if($_POST["password"] != $_POST["confirmpassword"]) 
-        {
-            $passwordMatchError ="<label>Password does not match</label>";
-        }
+        // if(empty($_POST["confirmpassword"]))  
+        // {  
+        //     $confirmpasswordError ="<label>Confirm Password Required</label>";  
+        // } 
+        // else if($_POST["password"] != $_POST["confirmpassword"]) 
+        // {
+        //     $passwordMatchError ="<label>Password does not match</label>";
+        // }
         else{
         //     if(file_exists("userInfo.json")){ 
         //         $current_data = file_get_contents('userInfo.json');
@@ -114,79 +114,65 @@
         <h1 style="text-align:center">Create Account</h1>
         <!-- <div style="width:500px">     -->
         <table>
-        <form action="#" method="post">
+        <form onsubmit= "return validate()"action="#" method="post">
                     <tr>
                     <td><label for="FirstName">First Name:</label></td>
                     <td><input type="text" name="firstname" id="firstname"value="<?php echo $firstName; ?>"><br><br></td>
-                        <?php
-                            if($firstNameError){
-                                echo "<span style='color:red'>$firstNameError</span><br><br>";
-                            }
-			            ?>
                         </tr>
                         <tr>
                         <td><label for="LastName">Last Name:</label></td>
                         <td><input type="text &nbsp" name="lastname" id="lastname"value="<?php echo $lastName;?>"><br><br></td>
-                        <?php
-                            if($lastNameError){
-                                echo "<span style='color:red'>$lastNameError</span><br><br>";
-                            }
-			            ?>
+                        
                         </tr>
 
                        <tr>
                        <td><label for="Email">Email:</label></td>
                        <td><input type="email" name="email" id="email"value="<?php echo $username;?>&nbsp"><br><br></td>
-                        <?php
-                            if($emailError){
-                                echo "<span style='color:red'>$emailError</span><br><br>";
-                            }
-                            else if($emailFormatError){
-                                    echo "<span style='color:red'>$emailFormatError</span><br><br>";
-                            }
-			            ?>
+                       
                        </tr>
                         <tr>
                             <td> <label for="Username">Username:</label></td>
                             <td><input type="text" name="username" id="username" value="<?php echo $confirmpassword; ?>"><br><br></td>
-                        <?php
-                            if($usernameError){
-                                echo "<span style='color:red'>$usernameError</span><br><br>";
-                            }
-			            ?>
+                        
                         </tr>
                         <tr>
                         <td><label for="Password">Password:</label></td>
                         <td><input type="password" name="password" id="password"value="<?php echo $password; ?>"><br><br></td>
-                        <?php
-                            if($passwordError){
-                                echo "<span style='color:red'>$passwordError</span><br><br>";
-                            }
-			            ?>
+                        
                         </tr>
                         <tr>
                         <td><label for="ConfirmPassword">Confirm Password:</label></td>
                          <td><input type="password" name="confirmpassword" id="confirmpassword" value="<?php echo $confirmpassword; ?>"><br><br></td>
-                         <?php
-                            if($confirmpasswordError){
-                                echo "<span style='color:red'>$confirmpasswordError</span><br><br>";
-                            }
-                            else if ($passwordMatchError){
-                                echo "<span style='color:red'>$passwordMatchError</span><br><br>";
-                            }
-			            ?>
+                         
                         </tr>
 
                        <tr>
                       <td><input type="submit" value="Submit" name="btnClick"></td>
-                        <?php
-                        if(isset($message))  
-                        {  
-                            echo $message;  
-                        }  
-                        ?>
                        </tr>
-                </form>
+                       </form>
         </table>
-    </body>
+                       <!-- <script>
+                           function validate(){
+                               var firstname = document.getElementById('firstname');
+                               var lastname = document.getElementById('lastname');
+                               var email = document.getElementById('email');
+                               var username = document.getElementById('username');
+                               var password = document.getElementById('password');
+                               var confirmpassword = document.getElementById('confirmpassword');
+
+                               if(firstname.value==""||lastname.value==""||email.value==""||username.value==""||password.value==""||confirmpassword.value==""||){
+                                   alert('Firstname cannot be empty');
+                                   alert('Lastname cannot be empty');
+                                   alert('Email cannot be empty');
+                                   alert('Username cannot be empty');
+                                   alert('Password cannot be empty');
+                                   alert('Confirm Password cannot be empty');
+                                   return false;
+                               }
+                               else{
+                                    return true;
+                               }
+                           }
+                       </script> -->
+          </body>
 </html>
