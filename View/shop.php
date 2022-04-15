a7ec01a9afb1c5e73a3a706244135a3cbedc754f
= <rakib.ivan100@gmail.com>
2022-04-15 16:40:09 +0600

    shop css added

---
+++ View/shop.php
@@ -0,0 +1,82 @@
+<html>
+    <head>
+        <title>Shop Now</title>
+    <link rel="stylesheet" href="../View/css/shop.css">
+    </head>
+    <body>
+    <section class="featured" id="featured">
+
+<h1 class="heading"> <i class="fas fa-paw"></i> Our Featured Products <i class="fas fa-paw"></i> </h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <div class="price">150taka</div>
+        <img src="../View/images/p1.webp" alt="" id="img">
+        <div class="content">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h3>Tasty pets foods</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, facilis?</p>
+            <a href="#" class="btn">Add to cart</a>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="price">600taka</div>
+        <img src="images/p2.webp" alt="" id="img">
+        <div class="content">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h3>Tasty pets foods</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, facilis?</p>
+            <a href="#" class="btn">Add to cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="price">450taka</div>
+        <img src="../View/images/p3.webp" alt="" id="img" id="img">
+        <div class="content">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h3>Tasty pets foods</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, facilis?</p>
+            <a href="#" class="btn">Add to cart</a>
+        </div>
+    </div>
+    <div class="box">
+        <div class="price">700taka</div>
+        <img src="../View/images/p4.webp" alt="" id="img">
+        <div class="content">
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <h3>Tasty pets foods</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, facilis?</p>
+            <a href="#" class="btn">Add to cart</a>
+        </div>
+    </div>
+</div>
+
+</section>
+</body>
+</html>
